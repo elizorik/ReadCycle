51773107ecd96d5ec8364935204dfd06d720ae5e
elizorik <elizaveta.sk24@gmail.com>
2025-04-23 21:23:04 +0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.readcycle
+
+import android.app.Application
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.readcycle.login.LoginScreen
+import com.example.readcycle.ui.theme.ReadCycleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginScreen()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ReadCycleTheme {
+        Greeting("Android")
+    }
+}
+//class App : Application() {
+//
+//    init {
+//        APP = this
+//    }
+//
+//    companion object {
+//        lateinit var APP: App
+//    }
+//}
